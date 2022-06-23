90270abbe24ea4893fb4b8a1b7d30a6ca7e2b57f
Thiago Paiva <thiagohdaqw@gmail.com>
2022-06-23 17:04:00 -0300

    Added map data and renderization

--- app/Main.hs
+++ app/Main.hs
@@ -2,48 +2,87 @@ module Main where
 
 import Graphics.Gloss
 import Graphics.Gloss.Interface.Pure.Game
+import Data.Fixed
 
-windowDisplay :: Display
-windowDisplay = InWindow "Window" (600, 600) (600, 600)
+import World
 
 type Node = (Float, Float)
-type Head = (Float, Float, Float, Float, [Node])
+type Head = (Float, Float, Float, Float)
+type Body = [Node]
+type Snake = (Head, Body)
+type Game = (Map, Snake) 
 
-block = 50
-fps = 5
+fps = 2
+
+width = 800     :: Int
+height = 600    :: Int
+blockSize = 50  :: Float
+
+startX = (blockSize - fromIntegral width + 1) / 2 
+startY = (fromIntegral height - blockSize - 1 ) / 2
+
+offset = blockSize/2
+
+game :: Game
+game = (World.mapData, ((0, 0, 0, 0), []))
 
 main :: IO ()
 main = play
-    windowDisplay
+    (InWindow "Snake Map" (width, height) (600, 600))
     white
     fps
-    (0, 0, 0, 0, [])
+    game
     drawingFunc
     inputHandler
     updateFunc
 
-drawNode :: Node -> Picture
-drawNode (x, y) = translate x y (color red $ rectangleWire block block)
 
-drawingFunc :: Head -> Picture
-drawingFunc (x, y, _, _, body) = pictures $ drawNode (x, y) : map drawNode body
+-- Draw
+colorCell 0 = red
+colorCell 1 = black
 
-inputHandler :: Event -> Head -> Head
-inputHandler (EventKey (SpecialKey KeyUp) Down _ _)     (x, y, _, _, body) = (x, y, 0,  1, body)
-inputHandler (EventKey (SpecialKey KeyDown) Down _ _)   (x, y, _, _, body) = (x, y, 0, -1, body)
-inputHandler (EventKey (SpecialKey KeyRight) Down _ _)  (x, y, _, _, body) = (x, y, 1,  0, body)
-inputHandler (EventKey (SpecialKey KeyLeft) Down _ _)   (x, y, _, _, body) = (x, y, -1, 0, body)
-inputHandler (EventKey (Char 'x') Down _ _)             (x, y, xx, yy, []) = (x, y, xx, yy, [(x, y)])
-inputHandler (EventKey (Char 'x') Down _ _)             (x, y, xx, yy, body) = (x, y, xx, yy, body ++ [last body])
-inputHandler _ w = w
+drawMapCell :: Float -> Float -> Cell -> Picture
+drawMapCell x y cell = translate x y $ color (colorCell cell) $ rectangleSolid blockSize blockSize
 
-updateFunc :: Float -> Head -> Head
-updateFunc dt (x, y, velx, vely, body) = (newX, newY, velx, vely, newBody)
+drawMap :: Map -> Float -> Float -> [Picture]
+drawMap [] _ _ = []
+drawMap (h:t) x y = drawMapCell x y h : drawMap t (nextX x) (nextY y)
     where
-        newX = x + velx*block
-        newY = y + vely*block
+        nextX xx
+            | mod' (xx + blockSize) 800 == 0 = 0
+            | otherwise = xx + blockSize
+        nextY yy
+            | mod' (x + blockSize) 800 == 0 = yy - blockSize
+            | otherwise = yy
+
+drawNode :: Node -> Picture
+drawNode (x, y) = translate x y (color blue $ rectangleSolid blockSize blockSize)
+
+drawSnake :: Snake -> [Picture]
+drawSnake ((x, y, _, _), body) = drawNode (x, y) : map drawNode body
+
+drawingFunc :: Game -> Picture
+drawingFunc (mapData, snake) = translate startX startY (pictures $ (drawMap mapData 0 0) ++ drawSnake snake)
+
+-- Update
+updateFunc :: Float -> Game -> Game
+updateFunc dt (mapData, ((x, y, velx, vely), body)) = (mapData, ((newX, newY, velx, vely), newBody))
+    where
+        newX = x + velx*blockSize
+        newY = y + vely*blockSize
         newBody = updateBody (x, y) body
 
         updateBody :: Node -> [Node] -> [Node]
         updateBody _ []   = []
         updateBody (x, y) (h:t) = (x, y) : updateBody h t
+
+
+-- Inputs
+inputHandler :: Event -> Game -> Game
+inputHandler (EventKey (SpecialKey KeyUp) Down _ _)     (mapData, ((x, y, _, _), body)) = (mapData, ((x, y, 0,  1), body))
+inputHandler (EventKey (SpecialKey KeyDown) Down _ _)   (mapData, ((x, y, _, _), body)) = (mapData, ((x, y, 0,  -1), body))
+inputHandler (EventKey (SpecialKey KeyRight) Down _ _)  (mapData, ((x, y, _, _), body)) = (mapData, ((x, y, 1,  0), body))
+inputHandler (EventKey (SpecialKey KeyLeft) Down _ _)   (mapData, ((x, y, _, _), body)) = (mapData, ((x, y, -1,  0), body))
+inputHandler (EventKey (Char 'x') Down _ _)             (mapData, ((x, y, xx, yy), [])) = (mapData, ((x, y, xx,  yy), [(x, y)]))
+inputHandler (EventKey (Char 'x') Down _ _)             (mapData, ((x, y, xx, yy), body)) = (mapData, ((x, y, xx, yy), body ++ [last body]))
+inputHandler _ w = w
